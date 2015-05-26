f57864f989f6cdb44a8d2bc89b65c5e1325c589b
bumbleblym <bumbleblym@gmail.com>
2015-05-27 01:57:06 +0800

    Update form to insert message

---
+++ app/client/templates/views/channel/messageForm/messageForm.js
@@ -0,0 +1,18 @@
+Template.messageForm.events({
+  'submit form': function(event, instance) {
+    event.preventDefault();
+
+    var input = event.target.content;
+    var content = input.value.trim();
+    input.value = '';
+
+    if (content !== '') {
+      Messages.insert({
+        userId: Meteor.userId(),
+        channel: Router.current().params.name,
+        content: content,
+        createdAt: new Date()
+      });
+    }
+  }
+});

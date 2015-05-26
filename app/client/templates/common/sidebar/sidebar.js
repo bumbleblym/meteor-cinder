574834f699da455122f81eeed3068c658bcf0642
bumbleblym <bumbleblym@gmail.com>
2015-05-27 00:23:14 +0800

    Change helper to return channels

--- app/client/templates/common/sidebar/sidebar.js
+++ app/client/templates/common/sidebar/sidebar.js
@@ -1,13 +1,5 @@
 Template.sidebar.helpers({
   channels: function() {
-    var channels = [];
-
-    _(10).times(function() {
-      channels.push({
-        name: Fake.word().toLowerCase()
-      });
-    });
-
-    return channels;
+    return Channels.find();
   }
 });

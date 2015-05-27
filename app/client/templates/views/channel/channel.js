17bdbbee4eae9a1d489b44e7cd2837fd0207d8a7
bumbleblym <bumbleblym@gmail.com>
2015-05-27 17:51:47 +0800

    Make form fixed to bottom

--- app/client/templates/views/channel/channel.js
+++ app/client/templates/views/channel/channel.js
@@ -1,3 +1,21 @@
+function setMessagesHeight() {
+  var navbarHeight = 50,
+    $main = $('.main'),
+    headerHeight = 50,
+    formHeight = $main.children('form').outerHeight(true),
+    $messages = $main.children('.messages');
+
+  console.log(headerHeight);
+  console.log(formHeight);
+  console.log($(window).height());
+
+  $messages.css('height', ($(window).height() - navbarHeight - headerHeight - formHeight) + 'px');
+}
+
+Template.channel.onRendered(function() {
+    setMessagesHeight();
+});
+
 Template.channel.helpers({
   channel: function() {
     return Channels.findOne({
